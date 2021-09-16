ec0602ff1c30ba57900c818d7078557bc6525fcd
J. B. Rainsberger <me@jbrains.ca>
2021-09-16 11:53:34 -0300

    Simplified the tests.

--- app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -6,14 +6,12 @@ import org.junit.jupiter.api.Test;
 public class AddFractionsTest {
     @Test
     void zeroPlusZero() {
-        Fraction sum = new Fraction(0).plus(new Fraction(0));
-        Assertions.assertEquals(0, sum.intValue());
+        Assertions.assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue());
     }
 
     @Test
     void notZeroPlusZero() {
-        Fraction sum = new Fraction(4).plus(new Fraction(0));
-        Assertions.assertEquals(4, sum.intValue());
+        Assertions.assertEquals(4, new Fraction(4).plus(new Fraction(0)).intValue());
     }
 
     private static class Fraction {
