8978e2a94379fabdec3e0250df549cc7eb20c32f
J. B. Rainsberger <me@jbrains.ca>
2021-09-20 10:41:40 -0300

    Created a new scope to help us focus on the part of the design we're actively refactoring.

--- app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -54,12 +54,14 @@ public class SellOneItemTest {
         public void onBarcode(String barcode) {
             if ("".equals(barcode))
                 display.setText("Scanning error: empty barcode");
-            else if ("12345".equals(barcode))
-                display.setText("EUR 7.95");
-            else if ("23456".equals(barcode))
-                display.setText("EUR 12.50");
-            else
-                display.setText(String.format("Product not found: %s", barcode));
+            else {
+                if ("12345".equals(barcode))
+                    display.setText("EUR 7.95");
+                else if ("23456".equals(barcode))
+                    display.setText("EUR 12.50");
+                else
+                    display.setText(String.format("Product not found: %s", barcode));
+            }
         }
     }
 
