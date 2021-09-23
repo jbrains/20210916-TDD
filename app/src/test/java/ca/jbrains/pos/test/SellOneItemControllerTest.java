2780be9e342891732925dc679d71d16ce42fb8b6
J. B. Rainsberger <me@jbrains.ca>
2021-09-23 09:32:17 -0300

    The Controller gracefully handles the case where the Catalog can't find a price.

--- app/src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
+++ app/src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
@@ -17,12 +17,27 @@ public class SellOneItemControllerTest {
         Mockito.verify(display).displayPrice(matchingPrice);
     }
 
+    @Test
+    void productNotFound() {
+        Catalog catalog = Mockito.mock(Catalog.class);
+        Display display = Mockito.mock(Display.class);
+
+        String missingBarcode = "::missing barcode::";
+        Mockito.when(catalog.findPrice(missingBarcode)).thenReturn(null);
+
+        new SellOneItemController(catalog, display).onBarcode(missingBarcode);
+
+        Mockito.verify(display).displayProductNotFoundMessage(missingBarcode);
+    }
+
     public interface Catalog {
         Price findPrice(String barcode);
     }
 
     public interface Display {
         void displayPrice(Price price);
+
+        void displayProductNotFoundMessage(String missingBarcode);
     }
 
     private static class Price {
@@ -41,7 +56,11 @@ public class SellOneItemControllerTest {
         }
 
         public void onBarcode(String barcode) {
-            display.displayPrice(catalog.findPrice(barcode));
+            Price price = catalog.findPrice(barcode);
+            if (price == null)
+                display.displayProductNotFoundMessage(barcode);
+            else
+                display.displayPrice(price);
         }
     }
 }
